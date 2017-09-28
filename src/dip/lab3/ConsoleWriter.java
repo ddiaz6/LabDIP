2ec7f55b611b1b80950d6be1927a2858b28d9c78
HP <HP@x360>
2017-09-27 23:18:12 -0500

    added a console Writer and added StartUp class

---
+++ src/dip/lab3/ConsoleWriter.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab3;
+import java.util.Scanner;
+
+/**
+ *
+ * @author HP
+ */
+public class ConsoleWriter {
+    Scanner keyboard = new Scanner(System.in);
+    String text= keyboard.nextLine();
+
+    
+    System.out.println(text);    
+}
